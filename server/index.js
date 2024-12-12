3e247cd439d0e3c84d23bd6db23a84d8fe620cb3
Harry Corrigan <corriganharry@outlook.com>
2024-12-12 16:23:05 +0000

    Trust proxy

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 const app = express()
 const port = 3000
+app.enable("trust proxy")
 
 app.use( bodyParser.json() );
 app.use(express.json({"limit": "5mb"}));
